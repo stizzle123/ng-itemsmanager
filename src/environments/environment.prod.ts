1037d51c64e98dd2e95b99822308da2d23e8f925
stizzle123 <arifayanidowu2@yahoo.com>
2019-06-28 12:00:49 +0100

    Updated environments.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAPMMV6rrmg8vM9HJDvae-PWu1LojXUNss",
+    authDomain: "itemsmanager-128e9.firebaseapp.com",
+    databaseURL: "https://itemsmanager-128e9.firebaseio.com",
+    projectId: "itemsmanager-128e9",
+    storageBucket: "itemsmanager-128e9.appspot.com",
+    messagingSenderId: "876742803957",
+    appId: "1:876742803957:web:d3d9a521d3312295"
+  }
 };
